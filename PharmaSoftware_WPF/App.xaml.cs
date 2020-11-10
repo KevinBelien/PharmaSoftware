dd1befa59ffaca0cd22783aa50fc62d23cdc6148
KevinBelien <K3vin.belien@gmail.com>
2020-11-10 19:17:47 +0100

    Initial Create

---
+++ PharmaSoftware_WPF/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PharmaSoftware_WPF.ViewModels;
+using PharmaSoftware_WPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PharmaSoftware_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LoginView mainView = new LoginView();
+            LoginViewModel mainViewModel = new LoginViewModel();
+            mainView.DataContext = mainViewModel;
+            mainView.Show();
+        }
+    }
+}

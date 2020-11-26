cd02ef08f4b51158583fe35e5341beeb8d62bfce
KevinBelien <K3vin.belien@gmail.com>
2020-11-26 13:17:06 +0100

    initialize storage view till expander

--- PharmaSoftware_WPF/ViewModels/RegisterViewModel.cs
+++ PharmaSoftware_WPF/ViewModels/RegisterViewModel.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PharmaSoftware_WPF.ViewModels
 {
-    public class RegisterViewModel
+    public class RegisterViewModel: BaseViewModel
     {
+        public override string this[string columnName] => throw new NotImplementedException();
+
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            switch (parameter.ToString())
+            {
+                case "CloseApp": Application.Current.Shutdown(); break;
+            }
+        }
     }
 }
